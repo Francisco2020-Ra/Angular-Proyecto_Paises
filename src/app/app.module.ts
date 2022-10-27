629bd8921eb35f31fa65fc1414bb393676b0a614
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-10-27 20:46:34 -0300

    modularizando el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
     SharedModule,
     PaisModule,
     
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Pais } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
-  constructor() { }
+  termino: string = 'Hola mundo'
+  hayError: boolean = false;
+  paises: Pais[] =[];
+
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
   }
 
+  buscar(termino: string){
+    this.hayError = false;
+    this.termino = termino;
+    console.log(this.termino);
+
+
+    this.paisService.buscarPais(this.termino).subscribe({
+      next: (paises) => {this.paises = paises; console.log(paises)},
+      error: (error) => {this.hayError = true; this.paises = [];}
+    });
+  }
+
+  
 }
